a8f3b84239e3648da0bf5015291bcbb36ed76539
Debbrata saha <debbrata@unm.edu>
2018-10-25 16:03:00 -0600

    upload all files

---
+++ local.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import json
+import sys
+from tsneFunctions import normalize_columns, tsne, master_child, listRecursive
+from tsneFunctions import demeanL
+
+
+def local_noop(args):
+    input_list = args["input"]
+    #a = 5
+    #raise Exception(a)
+    #raise Exception( 'local_noop function startings')
+    computation_output = {
+        "output": {
+            "computation_phase": 'local_noop',
+            "no_dims": input_list["no_dims"],
+            "initial_dims": input_list["initial_dims"],
+            "perplexity": input_list["perplexity"],
+            "max_iterations": input_list["max_iterations"]
+        },
+        "cache": {
+            "no_dims": input_list["no_dims"],
+            "initial_dims": input_list["initial_dims"],
+            "perplexity": input_list["perplexity"]
+        }
+    }
+
+    return json.dumps(computation_output)
+
+
+def local_1(args):
+    ''' It will load local data and download remote data and
+    place it on top. Then it will run tsne on combined data(shared + local)
+    and return low dimensional shared Y and IY
+
+       args (dictionary): {
+           "shared_X" (str): file path to remote site data,
+           "shared_Label" (str): file path to remote site labels
+           "no_dims" (int): Final plotting dimensions,
+           "initial_dims" (int): number of dimensions that PCA should produce
+           "perplexity" (int): initial guess for nearest neighbor
+           "shared_Y" (str):  the low-dimensional remote site data
+           }
+
+
+       Returns:
+           computation_phase(local): It will return only low dimensional
+           shared data from local site
+           computation_phase(final): It will return only low dimensional
+           local site data
+           computation_phase(computation): It will return only low
+           dimensional shared data Y and corresponding IY
+       '''
+
+    # corresponds to local
+    #raise Exception( 'local_1 function startings')
+
+    #shared_X = np.loadtxt('test/input/simulatorRun/shared_x.txt')
+    shared_X = np.loadtxt('test/input/simulatorRun/mnist2500_X.txt')
+    #shared_X = np.loadtxt('test/input/simulatorRun/test_high_dimensional_mnist_data.txt')
+    shared_Y = np.array(args["input"]["shared_y"])
+    #raise Exception(shared_Y.shape)
+    no_dims = args["cache"]["no_dims"]
+    initial_dims = args["cache"]["initial_dims"]
+    perplexity = args["cache"]["perplexity"]
+    sharedRows, sharedColumns = shared_X.shape
+
+    #Site1Data = np.loadtxt('test/input/simulatorRun/site1_x.txt')
+    #Site1Data = np.loadtxt('test/input/simulatorRun/site1_x_high_dimensions.txt')
+    Site1Data = np.loadtxt('test/input/simulatorRun/test_high_dimensional_site_1_mnist_data.txt')
+
+    # create combinded list by local and remote data
+    combined_X = np.concatenate((shared_X, Site1Data), axis=0)
+    combined_X = normalize_columns(combined_X)
+
+    # create low dimensional position
+    combined_Y = np.random.randn(combined_X.shape[0], no_dims)
+    combined_Y[:shared_Y.shape[0], :] = shared_Y
+
+
+    local_Y, local_dY, local_iY, local_gains, local_P, local_n = tsne(
+        combined_X,
+        combined_Y,
+        sharedRows,
+        no_dims=no_dims,
+        initial_dims=initial_dims,
+        perplexity=perplexity,
+        computation_phase="local")
+    local_shared_Y = local_Y[:shared_Y.shape[0], :]
+    local_shared_IY = local_iY[:shared_Y.shape[0], :]
+    #raise Exception(np.shape(local_shared_Y))
+    #raise Exception(sys.getsizeof(local_shared_Y))
+
+
+    computation_output = \
+        {
+            "output": {
+                "localSite1SharedY": local_shared_Y.tolist(),
+                "localSite1SharedIY": local_shared_IY.tolist(),
+                'computation_phase': "local_1"
+            },
+            "cache": {
+                "local_Y": local_Y.tolist(),
+                "local_dY": local_dY.tolist(),
+                "local_iY": local_iY.tolist(),
+                "local_P": local_P.tolist(),
+                "local_n": local_n,
+                "local_gains": local_gains.tolist(),
+                "shared_rows": sharedRows,
+                "shared_y": shared_Y.tolist()
+            }
+        }
+
+
+
+    return json.dumps(computation_output)
+
+
+
+def local_2(args):
+
+
+    iter = args["input"]["number_of_iterations"]
+    if (iter == 1):
+        raise Exception('I am in local_2 funcation somehow. Iteration no : ', iter)
+
+    local_sharedRows = args["cache"]["shared_rows"]
+    shared_Y = np.array(args["cache"]["shared_y"])
+
+    compAvgError1 = args["input"]["compAvgError"]
+    local_Y = np.array(args["cache"]["local_Y"])
+    local_dY = np.array(args["cache"]["local_dY"])
+    local_IY = np.array(args["cache"]["local_iY"])
+    local_P = np.array(args["cache"]["local_P"])
+    local_n = args["cache"]["local_n"]
+    local_gains = np.array(args["cache"]["local_gains"])
+
+    shared_Y = np.array(args["input"]["shared_Y"])
+    #iter = args["input"]["number_of_iterations"]
+
+    a,b = shared_Y.shape
+    c,d = local_Y.shape
+    #raise Exception( a, b,c,d,local_sharedRows)
+
+    #It should be the average one
+    local_Y[:local_sharedRows, :] = shared_Y
+    C = compAvgError1['error']
+    demeanAvg = (np.mean(local_Y, 0))
+    demeanAvg[0] = compAvgError1['avgX']
+    demeanAvg[1] = compAvgError1['avgY']
+    local_Y = demeanL(local_Y, demeanAvg)
+
+    local_Y, dY, local_IY, gains, n, sharedRows, P, C = master_child( local_Y, local_dY, local_IY, local_gains, local_n, local_sharedRows,local_P, iter, C)
+
+    local_Y[local_sharedRows:, :] = local_Y[local_sharedRows:, :] + local_IY[local_sharedRows:, :]
+
+    local_Shared_Y = local_Y[:local_sharedRows, :]
+    local_Shared_IY = local_IY[:local_sharedRows, :]
+    meanValue = (np.mean(local_Y, 0))
+    #local_Y_labels = np.loadtxt('test/input/simulatorRun/site1_y.txt')  ## there is problem here
+    local_Y_labels = np.loadtxt('test/input/simulatorRun/test_high_dimensional_site_1_mnist_label.txt')  ## there is problem here
+    #local_Y_labels = np.loadtxt('test/input/simulatorRun/site1_high_dimensional_label.txt')  ## there is problem here
+
+
+    if iter > 2:
+        #local_Y_labels = np.loadtxt('test/input/simulatorRun/site1_y.txt')  ## there is problem here
+        #local_Y_labels = np.loadtxt('test/input/simulatorRun/site1_high_dimensional_label.txt')  ## there is problem here
+        local_Y_labels = np.loadtxt('test/input/simulatorRun/test_high_dimensional_site_1_mnist_label.txt')  ## there is problem here
+        computation_output = {
+            "output": {
+                "MeanX": meanValue[0],
+                "MeanY": meanValue[1],
+                "error": C,
+                "local_Shared_iY": local_Shared_IY.tolist(),
+                "local_Shared_Y": local_Shared_Y.tolist(),
+                "local_Y": local_Y[local_sharedRows:, :].tolist(),
+                "local_Y_labels": local_Y_labels.tolist(),
+                "computation_phase": "local_2"
+        },
+
+        "cache": {
+            "local_Y": local_Y.tolist(),
+            "local_dY": local_dY.tolist(),
+            "local_iY": local_IY.tolist(),
+            "local_P": P.tolist(),
+            "local_n": n,
+            "local_gains": local_gains.tolist(),
+            "shared_rows": sharedRows,
+            "shared_y": local_Shared_Y.tolist()
+        }
+        }
+
+    ## there is problem here
+    else:
+        computation_output = {
+            "output": {
+                "MeanX": meanValue[0],
+                "MeanY": meanValue[1],
+                "error": C,
+                "local_Shared_iY": local_Shared_IY.tolist(),
+                "local_Shared_Y": local_Shared_Y.tolist(),
+                "local_Y": local_Y[local_sharedRows:, :].tolist(),
+                "local_Y_labels": local_Y_labels.tolist(),
+                "computation_phase": "local_2"
+            },
+            "cache": {
+                "local_Y": local_Y.tolist(),
+                "local_dY": local_dY.tolist(),
+                "local_iY": local_IY.tolist(),
+                "local_P": P.tolist(),
+                "local_n": n,
+                "local_gains": local_gains.tolist(),
+                "shared_rows": sharedRows,
+                "shared_y": local_Shared_Y.tolist()
+            }
+        }
+
+
+    return json.dumps(computation_output)
+
+
+def local_3(args):
+    # corresponds to final
+    return 0
+
+
+if __name__ == '__main__':
+    np.random.seed(0)
+
+    parsed_args = json.loads(sys.stdin.read())
+    phase_key = list(listRecursive(parsed_args, 'computation_phase'))
+
+
+
+    if not phase_key:
+        computation_output = local_noop(parsed_args)
+        sys.stdout.write(computation_output)
+    elif 'remote_1' in phase_key:
+        computation_output = local_1(parsed_args)
+
+        sys.stdout.write(computation_output)
+    elif 'remote_2' in phase_key:
+        computation_output = local_2(parsed_args)
+        sys.stdout.write(computation_output)
+    elif 'remote_3' in phase_key:
+        computation_output = local_3(parsed_args)
+        sys.stdout.write(computation_output)
+    else:
+        raise ValueError("Error occurred at Local")
---
+++ remote.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+from tsneFunctions import normalize_columns, tsne, listRecursive
+import json
+
+
+def remote_1(args):
+    ''' It will receive parameters from dsne_multi_shot.
+    After receiving parameters it will compute tsne on high
+    dimensional remote data and pass low dimensional values
+    of remote site data
+
+
+       args (dictionary): {
+            "shared_X" (str):  remote site data
+            "shared_Label" (str): remote site labels
+            "no_dims" (int): Final plotting dimensions
+            "initial_dims" (int): number of dimensions that PCA should produce
+            "perplexity" (int): initial guess for nearest neighbor
+            "max_iter" (str):  maximum number of iterations during
+                                tsne computation
+            }
+       computation_phase (string): remote
+
+       normalize_columns:
+           Shared data is normalized through this function
+
+       Returns:
+           Return args will contain previous args value in
+           addition of Y[low dimensional Y values] values of shared_Y.
+       args(dictionary):  {
+           "shared_X" (str):  remote site data,
+           "shared_Label" (str):  remote site labels
+           "no_dims" (int): Final plotting dimensions,
+           "initial_dims" (int): number of dimensions that PCA should produce
+           "perplexity" (int): initial guess for nearest neighbor
+           "shared_Y" : the low-dimensional remote site data
+           }
+       '''
+
+    shared_X = np.loadtxt('test/input/simulatorRun/mnist2500_X.txt')
+    shared_Labels = np.loadtxt('test/input/simulatorRun/mnist2500_labels.txt')
+    #shared_X = np.loadtxt('test/input/simulatorRun/shared_x.txt')
+    #shared_Labels = np.loadtxt('test/input/simulatorRun/shared_y.txt')
+    #shared_X = np.loadtxt('test/input/simulatorRun/test_high_dimensional_mnist_data.txt')
+    #shared_Labels = np.loadtxt('test/input/simulatorRun/test_high_dimensional_mnist_label.txt')
+
+    no_dims = args["input"]["local0"]["no_dims"]
+    initial_dims = args["input"]["local0"]["initial_dims"]
+    perplexity = args["input"]["local0"]["perplexity"]
+    max_iter = args["input"]["local0"]["max_iterations"]
+
+    shared_X = normalize_columns(shared_X)
+    (sharedRows, sharedColumns) = shared_X.shape
+
+    init_Y = np.random.randn(sharedRows, no_dims)
+
+    shared_Y = tsne(
+        shared_X,
+        init_Y,
+        sharedRows,
+        no_dims,
+        initial_dims,
+        perplexity,
+        computation_phase="remote")
+    #raise Exception(shared_X)
+
+
+
+    computation_output = {
+        "output": {
+            "shared_y": shared_Y.tolist(),
+            "computation_phase": 'remote_1',
+        },
+        "cache": {
+            "shared_y": shared_Y.tolist(),
+            "max_iterations": max_iter
+        }
+    }
+    #raise Exception(shared_Y.shape)
+    return json.dumps(computation_output)
+
+
+def remote_2(args):
+    '''
+    args(dictionary):  {
+        "shared_X"(str): remote site data,
+        "shared_Label"(str): remote site labels
+        "no_dims"(int): Final plotting dimensions,
+        "initial_dims"(int): number of dimensions that PCA should produce
+        "perplexity"(int): initial guess for nearest neighbor
+        "shared_Y": the low - dimensional remote site data
+
+    Returns:
+        Y: the final computed low dimensional remote site data
+        local1Yvalues: Final low dimensional local site 1 data
+        local2Yvalues: Final low dimensional local site 2 data
+    }
+    '''
+
+    Y =  np.array(args["cache"]["shared_y"])
+    #raise Exception(Y)
+    average_Y = (np.mean(Y, 0))
+    average_Y[0] = 0
+    average_Y[1] = 0
+    C = 0
+
+    compAvgError = {'avgX': average_Y[0], 'avgY': average_Y[1], 'error': C}
+
+    computation_output = \
+    {
+        "output": {
+            "compAvgError": compAvgError,
+            "computation_phase": 'remote_2',
+            "shared_Y": Y.tolist(),
+            "number_of_iterations": 0
+
+                },
+
+        "cache": {
+            "compAvgError": compAvgError,
+            "number_of_iterations": 0
+        }
+    }
+    #raise Exception(Y.shape)
+    return json.dumps(computation_output)
+
+
+def remote_3(args):
+
+    iteration =  args["cache"]["number_of_iterations"]
+    iteration +=1;
+    C = args["cache"]["compAvgError"]["error"]
+
+    #Y = args["input"]["local_Y"]
+
+    #average_Y = (np.mean(Y, 0))
+    average_Y = [0]*2
+    C = 0
+    #avg_beta_vector = np.mean([input_list[site]["beta_vector_local"] for site in input_list], axis=0)
+
+    average_Y[0] = np.mean([args['input'][site]['MeanX'] for site in args["input"]])
+    average_Y[1] = np.mean([args['input'][site]['MeanY'] for site in args["input"]])
+
+    average_Y = np.array(average_Y)
+    C = C + np.mean([args['input'][site]['error'] for site in args["input"]])
+
+    meanY = np.mean([args["input"][site]["local_Shared_Y"] for site in args["input"]], axis=0)
+    meaniY = np.mean([args["input"][site]["local_Shared_iY"] for site in args["input"]], axis=0)
+
+    Y = meanY + meaniY
+
+
+    Y -= np.tile(average_Y, (Y.shape[0], 1))
+
+    compAvgError = {'avgX': average_Y[0], 'avgY': average_Y[1], 'error': C}
+
+    #if(iteration == 20):
+        #raise Exception('In remote_3 after iterations 20')
+
+    if(iteration<200):
+        phase = 'remote_2';
+    else:
+        phase = 'remote_3';
+
+
+
+    if iteration == 3:
+        #raise Exception( 'During iteration 2 in line 173 of course in remote funcion')
+        shared_labels = np.loadtxt('test/input/simulatorRun/mnist2500_labels.txt')
+        #shared_labels = np.loadtxt('test/input/simulatorRun/shared_y.txt')
+        #shared_labels = np.loadtxt('test/input/simulatorRun/test_high_dimensional_mnist_label.txt')
+        concat_Y = []
+        concat_local_Y_labels = []
+
+        concat_Y.append(Y)
+        concat_local_Y_labels.append(shared_labels)
+        #concat_local_Y_labels =  np.concatenate(concat_local_Y_labels,shared_labels)
+
+
+        for site in args["input"]:
+            #raise Exception(type(args["input"][site]["local_Y"]))
+            #concat_Y.np.concatenate(args['input'][site]["local_Y"])
+            concat_Y.append(args["input"][site]["local_Y"])
+            concat_local_Y_labels.append(args["input"][site]["local_Y_labels"])
+
+            #np.concatenate( (concat_Y, args['input'][site]["local_Y"]),  axis=0 )
+            #np.concatenate((concat_local_Y_labels, args["input"][site]["local_Y_labels"]), axis=0)
+            #concat_local_Y_labels.np.concatenate(args["input"][site]["local_Y_labels"])
+
+        filepath = 'test/output/remote/simulatorRun/lowdimembed.txt'
+        f = open(filepath, 'w+')
+        for line1, line2 in zip(concat_Y, concat_local_Y_labels):
+            f.writelines(([ str(line1), str(line2)]))
+        f.close()
+        #concat_Y =  [int(i) for i in concat_Y]
+        #concat_local_Y_labels = [int(j) for j in concconcat_local_Y_labelsat_Y]
+
+        #raise Exception( concat_Y, concat_local_Y_labels)
+        #raise Exception(aaa)
+
+        #raise Exception( concat_Y, concat_local_Y_labels)
+
+
+
+    else:
+
+        computation_output = {"output": {
+                                "compAvgError": compAvgError,
+                                "number_of_iterations": 0,
+                                "shared_Y": Y.tolist(),
+                                "computation_phase": phase},
+
+                                "cache": {
+                                    "compAvgError": compAvgError,
+                                    "number_of_iterations": iteration
+                                }
+                            }
+
+
+    aSize = [0]*2
+    aSize[0] = sys.getsizeof(compAvgError);
+    aSize[1] = sys.getsizeof(Y.tolist());
+    totalSize = sum(aSize);
+    #raise Exception('I am in remote_3 funcation somehow. Total size : ', totalSize)
+    # The problem is happenning here. During passing 12 KB of data it is showing error
+
+    return json.dumps(computation_output)
+
+
+def remote_4(args):
+
+    # Final aggregation step
+    computation_output = {"output": {"final_embedding": 0}, "success": True}
+    return json.dumps(computation_output)
+
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+    parsed_args = json.loads(sys.stdin.read())
+
+    phase_key = list(listRecursive(parsed_args, 'computation_phase'))
+
+    if 'local_noop' in phase_key:
+        computation_output = remote_1(parsed_args)
+        sys.stdout.write(computation_output)
+    elif 'local_1' in phase_key:
+        computation_output = remote_2(parsed_args)
+        sys.stdout.write(computation_output)
+    elif 'local_2' in phase_key:
+        computation_output = remote_3(parsed_args)
+        sys.stdout.write(computation_output)
+    elif 'local_3' in phase_key:
+        computation_output = remote_4(parsed_args)
+        sys.stdout.write(computation_output)
+    else:
+        raise ValueError("Error occurred at Remote")
